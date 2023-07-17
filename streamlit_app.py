5dc63a11cc6f8d9d0098fec1e2d435ef6b8e9f85
commiunni <lee980323@gmail.com>
2023-07-17 17:42:06 +0900

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,15 @@ st.write(math.pi)
 
 
 with st.echo():
-    radius = st.number_input("반지름")
-    A = '원주율 : '
-    B = '반지름 : '
-    C = '원의 둘레 : '
-    D = '원의 넓이 : '
+import streamlit as st
 
-    st.write(q = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679)
-    st.write(r = 15)
+radius = st.number_input("반지름")
+pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 
-    st.write(print(A, q))
-    st.write(print(B, r))
-    st.write(print(C, r * 2 * q))
-    st.write(print(D, r * r * q))
+circumference = 2 * pi * radius
+area = pi * radius * radius
+
+st.write("원주율 :", pi)
+st.write("반지름 :", radius)
+st.write("원의 둘레 :", circumference)
+st.write("원의 넓이 :", area)
